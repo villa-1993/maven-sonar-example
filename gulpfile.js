62cad0b2166cdf302c6c8fa6fb240bea1d6f6b5c
JackyShen <mebusw@163.com>
2015-09-24 10:59:38 +0800

    append jasmine to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 var gulp = require('gulp');
+var jasmine = require('gulp-jasmine');
 
 gulp.task('default', function() {
   // place code for your default task here
   console.log('Hello World');
 });
 
+gulp.task('test', function () {
+    return gulp.src('src/test/webapp/test.js')
+        .pipe(jasmine());
+});
